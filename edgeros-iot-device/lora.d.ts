c4ba4422ca62b24f4ba7ed1e1b9b9cc39c48e5e9
xieyuanbin <xieyuanbin@acoinfo.com>
2021-04-09 19:56:50 +0800

    iot apis done

--- edgeros-iot-device/lora.d.ts
+++ edgeros-iot-device/lora.d.ts
@@ -4,14 +4,18 @@ declare module 'edgeros:lora' {
 }
 
 declare module 'lora' {
+  import { Buffer } from "edgeros:buffer";
   class LoRa {
     constructor();
+    static count(callback: (error: Error, count: number) => void): void;
+    static list(callback: (error: Error, list: Array<any>) => void): void;
     close(): void;
-    publish(loraid: string, data: Buffer): void;
-    publish(loraid: string, data: Buffer, req: boolean, callback?: (error: Error) => void): void;
-    subscribe(loraid?: string): void;
-    unsubscribe(loraid?: string): void;
-    on(event: 'message', callback: (loraid: string, data: Buffer, ack: boolean) => void): void;
+    publish(devEUI: string, data: Buffer): void;
+    publish(devEUI: string, data: Buffer, req: boolean, callback?: (error: Error) => void): void;
+    subscribe(devEUI?: string): void;
+    unsubscribe(devEUI?: string): void;
+    on(event: 'message', callback: (devEUI: string, data: Buffer) => void): void;
+    on(event: 'failed', callback: (devEUI: string, error: Error) => void): void;
   }
   export = LoRa
 }
