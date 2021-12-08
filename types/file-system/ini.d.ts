ecafdd83d99004e7ce8b932da3bc9bd4c555a41e
xieyuanbin <xieyuanbin@acoinfo.com>
2021-12-08 18:22:38 +0800

    Update global module INI

--- types/file-system/ini.d.ts
+++ types/file-system/ini.d.ts
@@ -1,28 +1,29 @@
 declare module 'edgeros:ini' {
-  import ini = require('ini');
-  export = ini;
+  import INI = require('ini');
+  export = INI;
 }
 
 declare module "ini" {
   type Replacer = (...args: any) => any;
-  namespace ini {
-    /**
-     * Parse the specified INI string and return the result object.
-     *
-     * @param iniString INI format string.
-     * @returns Convert to object.
-     */
-    function parse(iniString: string): object;
-
-    /**
-     * Convert specified object to INI format string. Similar to `JSON.stringify()`.
-     *
-     * @param obj object Object to be convert.
-     * @param replacer Unsupport now.
-     * @param space space Add indents, spaces, and line breaks to string. default: ''.
-     * @returns INI format string.
-     */
-    function stringify(obj: object, replacer?: Replacer | Replacer[], space?: string): string;
+  global {
+    const INI: {
+      /**
+       * Parse the specified INI string and return the result object.
+       *
+       * @param iniString INI format string.
+       * @returns Convert to object.
+       */
+      parse(iniString: string): object;
+      /**
+       * Convert specified object to INI format string. Similar to `JSON.stringify()`.
+       *
+       * @param obj object Object to be convert.
+       * @param replacer Unsupport now.
+       * @param space space Add indents, spaces, and line breaks to string. default: ''.
+       * @returns INI format string.
+       */
+      stringify(obj: object, replacer?: Replacer | Replacer[], space?: string): string;
+    };
   }
-  export = ini;
+  export = INI;
 }
